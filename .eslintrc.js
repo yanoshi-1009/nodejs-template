510e6958fece5506c81269806833276c5cc49d25
shohei-yano <shohei-yano@cybozu.co.jp>
2020-11-15 13:15:03 +0900

    fix settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
-  extends: '@cybozu/eslint-config/presets/prettier',
+  extends: '@cybozu/eslint-config/presets/node-prettier',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true
+    }
+  },
   globals: {
     kintone: false
   }
